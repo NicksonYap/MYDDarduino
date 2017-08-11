c5b3bc253ba43071b5d94530f8bbd6a8f0528baf
Nickson Yap <hi@nickson.me>
2017-08-12 01:29:28 +0800

    3d plot with serial input!!!

--- MATLAB/3Dplot.m
+++ MATLAB/3Dplot.m
@@ -1,9 +1,13 @@
 %prevents rounding when displaying fractions
 format long
 %Reader 3-D Coordinates [x, y, z]
-Reader = [-2, 2, 2; 2, 1, -2; -1, -2, 2; 3, 3, 3];
+Reader = [0,0,0; 0,0,242; 0,206,242; 422,206,242];
 %plot reader locations
-scatter3(getcolumn(Reader(1:4,1:3),1),getcolumn(Reader(1:4,1:3),2),getcolumn(Reader(1:4,1:3),3), 'MarkerEdgeColor', [1 0 0], 'MarkerFaceColor', [1 0 0]);figure(gcf)
+figure(gcf)
+scatter3(getcolumn(Reader(1:4,1:3),1),getcolumn(Reader(1:4,1:3),2),getcolumn(Reader(1:4,1:3),3), 'MarkerEdgeColor', [1 0 0], 'MarkerFaceColor', [1 0 0]);
+axis([-50 500 -50 500 -50 500]) %set axis for 2-D graphs
+rotate3d;
+drawnow;
 %x = 0;0;0;
 %y = 0;0;0;
 %x = getcolumn(Reader(1:3,1:2),1)
@@ -11,19 +15,49 @@ scatter3(getcolumn(Reader(1:4,1:3),1),getcolumn(Reader(1:4,1:3),2),getcolumn(Rea
 %e = [1;1;1]
 hold on
 %errorbar(x,y, e, 'og', 'Marker', '+');
-axis([-3 3 -3 3 -3 3]) %set axis for 2-D graphs
-set(gca, 'XTick', -3:1:3);
-set(gca, 'YTick', -3:1:3);
-set(gca, 'ZTick', -3:1:3);
-grid on;
-%distances to Tag from Reader(i)
-%Distance = [2.82842715;2.236067977;2.236067977]; %(0, 0) tag
-Distance = [3.464101615; 3; 3; 5.196152423]; %(0, 0 , 0) tag
-%initialize variables
-x = 0;
-y = 0;
-z = 0;
-[x, y, z] = three_tri(Reader(1, 1), Reader(1, 2), Reader(1, 3), Distance(1), Reader(2,1), Reader(2, 2), Reader(2, 3), Distance(2), Reader(3, 1), Reader(3, 2), Reader(3, 3),Distance(3), Reader(4, 1), Reader(4, 2), Reader(4, 3), Distance(4));
-scatter3(x, y, z, 'MarkerEdgeColor', [1 1 0], 'MarkerFaceColor', [1 1 0]);
+
 axis square 
-figure(gcf)
+%set(gca, 'XTick', -50:10:500);
+%set(gca, 'YTick', -50:10:500);
+%set(gca, 'ZTick', -50:10:500);
+grid on;
+
+%pause
+
+if(numel(instrfind) > 0)
+    fclose(instrfind); %close all ports before opening
+end
+
+s = serial('COM5');
+set(s,'Timeout',1);
+set(s,'DataBits',8);
+set(s,'StopBits',1);
+set(s,'BaudRate',115200);
+set(s,'Parity','none');
+s.ReadAsyncMode='continuous';
+fopen(s);
+drawLoc = 0;
+while(1)
+    f = fscanf(s, '%f,%f,%f,%f\n');
+    if(numel(f) == 4)
+       
+
+        %distances to Tag from Reader(i)
+        %Distance = [2.82842715;2.236067977;2.236067977]; %(0, 0) tag
+        %Distance = [3.464101615; 3; 3; 5.196152423]; %(0, 0 , 0) tag
+        %initialize variables
+        
+        Distance = f;
+        delete(findobj('MarkerEdgeColor',[0 0 1]))
+        if(f(1) ~= -1 && f(2) ~= -1 && f(3) ~= -1 && f(4) ~= -1)
+            [x, y, z] = three_tri(Reader(1, 1), Reader(1, 2), Reader(1, 3), Distance(1), Reader(2,1), Reader(2, 2), Reader(2, 3), Distance(2), Reader(3, 1), Reader(3, 2), Reader(3, 3),Distance(3), Reader(4, 1), Reader(4, 2), Reader(4, 3), Distance(4));
+            drawLoc = scatter3(x, y, z, 'MarkerEdgeColor', [0 0 1], 'MarkerFaceColor', [0 0 1]);
+            drawnow;
+        end
+       
+    end
+    disp(f);
+end
+fclose(s);
+fclose(instrfind); 
+
